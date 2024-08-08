2375adff3b65e922a03d5934e283c8b96da918b6
Oluwatodimu Adegoke <adegokeoluwatodimu@yahoo.com>
2024-08-07 23:05:06 -0500

    Added audio feature

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { Terminal } from "lucide-react";
 import { useState } from "react";
 import TrackCard from "@/components/ui/Cards";
 import Link from "next/link";
+import { LoadingSpinner } from "@/components/ui/Spinner";
 interface Track {
   title: string;
   artist_name: string;
@@ -16,6 +17,7 @@ interface Track {
   song_uri: string;
 }
 export default function Home() {
+  const [loading, setLoading] = useState(false);
   const [city, setCity] = useState("");
   const [tracks, setTracks] = useState([]);
   const [hide, setHide] = useState(true);
@@ -29,6 +31,7 @@ export default function Home() {
   };
   const getWeather = async () => {
     if (city !== "") {
+      setLoading(true);
       const response = await fetch(`/api/tracks`, {
         method: "POST",
         body: JSON.stringify({ city: city }),
@@ -41,16 +44,20 @@ export default function Home() {
     } else {
       showErrorMessage("City cannot be empty");
     }
+    setLoading(false);
   };
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 gap-2">
+    <main className="flex min-h-screen flex-col items-center px-24 gap-2">
+      <h1 className="font-bold text-6xl m-8 text-rose-400">Weather Beats</h1>
       <Alert hidden={hide} className="max-w-lg">
         <Terminal className="h-4 w-4" />
         <AlertTitle>Invalid City</AlertTitle>
         <AlertDescription>{description}</AlertDescription>
       </Alert>
-      <div className="flex flex-row items-center gap-2">
+
+      <div className="flex flex-row items-center gap-2 ">
         <Input
+          className="w-full"
           placeholder="City"
           value={city}
           onChange={(e) => setCity(e.target.value)}
@@ -59,7 +66,8 @@ export default function Home() {
           Search
         </Button>
       </div>
-      <div className="gap-4 flex flex-col">
+      {loading && <LoadingSpinner size={100} />}
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
         {tracks.length > 0 ? (
           tracks.map((track: Track) => <TrackCard track={track} />)
         ) : (
--- components/ui/Cards.tsx
+++ components/ui/Cards.tsx
@@ -1,46 +1,77 @@
 "use client";
 import { Track } from "@/lib/track";
-import { ChevronsRight } from "lucide-react";
+import { ChevronsRight, ExternalLink, Pause, Play } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { title } from "process";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { Button } from "./button";
+
 const TrackCard = ({ track }: { track: Track }) => {
-  const audioRef = useRef();
-  //   const play = () => {
-  //     if(audioRef.current){
-  //         audioRef.current.play();
-  //     }
-  //   }
-  //   const pause = () => {
-  //     if(audioRef.current){
-  //         audioRef.current.pause();
-  //     }
-  //   }
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const play = () => {
+    if (audioRef.current) {
+      setIsPlaying(true);
+      audioRef.current.play();
+    }
+  };
+
+  const pause = () => {
+    if (audioRef.current) {
+      setIsPlaying(false);
+      audioRef.current.pause();
+    }
+  };
 
   return (
-    <section className="flex flex-row items-center max-w-sm rounded-lg bg-primary-foreground justify-between">
-      <div className="flex flex-row items-center">
+    <section className="flex flex-row items-center max-w-sm rounded-lg bg-primary-foreground justify-between p-2 max-h-20">
+      <div className="flex flex-row items-center w-full">
         <div className="">
           <Image
             className="rounded-lg p-1"
             alt={"Track Picture"}
             src={track.image_url}
-            height={80}
-            width={80}
+            height={100}
+            width={100}
           />
         </div>
-        <div className="items-start flex flex-col justify-start align-top p-2">
-          <p className="flex text-sm font-bold ">{track.title}</p>
-          <p className="flex text-sm">{track.artist_name}</p>
-        </div>
-      </div>
+        <div className="items-start flex flex-col justify-start  w-full">
+          <div className="flex flex-row justify-between w-full">
+            <span className="flex flex-row gap-2">
+              <p className="text-sm font-bold">Track:</p>
+              <p className="flex text-sm font-semibold">
+                {track.title.substring(0, 30)}
+                {track.title.length > 30 && "..."}
+              </p>
+            </span>
 
-      <div className="flex self-end">
-        <audio src={track?.song_uri}></audio>
-        <Link href={track?.song_url}>
-          <ChevronsRight className="h-6 w-6" />
-        </Link>
+            {isPlaying && (
+              <p className="text-end text-xs text-red-600">Playing</p>
+            )}
+          </div>
+          <span className="flex flex-row gap-2">
+            <p className="text-sm font-bold">Artist:</p>
+            <p className="flex text-sm">{track.artist_name}</p>
+          </span>
+          <div className="flex items-center gap-2 flex-row justify-between w-full">
+            <div>
+              {track?.song_uri && (
+                <div className="flex flex-row items-center gap-2">
+                  <audio ref={audioRef} src={track?.song_uri}></audio>
+                  <Button onClick={play} size={"sm"} variant={"outline"}>
+                    <Play></Play>
+                  </Button>
+                  <Button onClick={pause} size={"sm"} variant={"outline"}>
+                    <Pause></Pause>
+                  </Button>
+                </div>
+              )}
+            </div>
+            <Link href={track?.song_url} className="flex ">
+              <ExternalLink className="h-6 w-6" />
+            </Link>
+          </div>
+        </div>
       </div>
     </section>
   );
